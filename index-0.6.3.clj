3a978378f37a14b2c377cff000171161fd4d10b5
Tom Faulhaber <git_net@infolace.com>
2012-11-14 22:02:34 -0800

    Autodoc commit for master/c61a736c

---
+++ index-0.6.3.clj
@@ -0,0 +1,9 @@
+{:namespaces
+ ({:source-url
+   "https://github.com/clojure/core.cache/blob/10cc84973a7b97ada8fb56e9859ce101a477bde5/src/main/clojure/clojure/core/cache.clj",
+   :wiki-url
+   "http://clojure.github.com/core.cache/clojure.core.cache-api.html",
+   :name "clojure.core.cache",
+   :author "Fogus",
+   :doc "A caching library for Clojure."}),
+ :vars ()}
