9f4b00e0a63d88800c7e4bf8577a8ac0677828a3
Fogus <mefogus@gmail.com>
2013-01-17 11:31:34 -0500

    Fixing up proj.clj

--- project.clj
+++ project.clj
@@ -2,14 +2,12 @@
   :description "Cache library for Clojure."
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
                  [org.clojure/data.priority-map "0.0.2"]]
-  :dev-dependencies [[jline "0.9.94"]
-                     [lein-marginalia "0.7.1"]
-                     [lein-multi "1.1.0"]]
-  :multi-deps {"1.2"   [[org.clojure/clojure "1.2.0"]]
-               "1.2.1" [[org.clojure/clojure "1.2.1"]]
-               "1.3"   [[org.clojure/clojure "1.3.0"]]
-               "1.4"   [[org.clojure/clojure "1.4.0"]]}
-  :plugins [[lein-swank "1.4.4"]]
+  :profiles {:1.2   {:dependencies [[org.clojure/clojure "1.2.0"]]}
+             :1.2.1 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3   {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :plugins [[lein-swank "1.4.4"]
+            [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :source-paths ["src/main/clojure"]
   :test-paths   ["src/test/clojure"])
