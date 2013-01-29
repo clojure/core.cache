6e4bbb08138a9e2d3363ef0792121e5207de490f
Tom Faulhaber <git_net@infolace.com>
2013-01-29 15:54:19 -0800

    Autodoc commit for master/9f4b00e0

--- index-0.6.3.clj
+++ index-0.6.3.clj
@@ -1,6 +1,6 @@
 {:namespaces
  ({:source-url
-   "https://github.com/clojure/core.cache/blob/10cc84973a7b97ada8fb56e9859ce101a477bde5/src/main/clojure/clojure/core/cache.clj",
+   "https://github.com/clojure/core.cache/blob/28918fe1a109cbfe4b9e985a79d39eb3a92a7389/src/main/clojure/clojure/core/cache.clj",
    :wiki-url
    "http://clojure.github.com/core.cache/clojure.core.cache-api.html",
    :name "clojure.core.cache",
