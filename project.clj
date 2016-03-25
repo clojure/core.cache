f4bc19e71d1ad637006fc5e4eb6f5e7ebf20caf5
fogus <mefogus@gmail.com>
2016-03-25 15:24:52 -0400

    Updating to latest version of pri-map.

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject core.cache "0.6.5-SNAPSHOT"
   :description "Cache library for Clojure."
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.priority-map "0.0.4"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.priority-map "0.0.7"]]
   :profiles {:1.2   {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.2.1 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3   {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5   {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6   {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
