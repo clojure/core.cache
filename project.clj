e2e5ef7cbd75717255dd62e810a1ee61311be868
Sean Corfield <sean@corfield.org>
2018-03-02 10:49:26 -0800

    Bump project.clj version for consistency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject core.cache "0.7.0"
+(defproject core.cache "0.7.1"
   :description "Cache library for Clojure."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.priority-map "0.0.7"]]
