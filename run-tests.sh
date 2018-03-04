291472b6791d77b936725342bd54b179925505d7
Sean Corfield <sean@corfield.org>
2018-03-03 21:31:26 -0800

    Bump to latest test-runner and simplify run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-#
-# We don't auto-test against Clojure 1.6 because the Cognitect test runner
-# requires at least 1.7, so we run those tests manually:
 
-time clj -A:1.6:test -e \
-"(require 'clojure.core.cache-test)\
- (clojure.test/run-tests 'clojure.core.cache-test)"
-
-versions="1.7 1.8 1.9 master"
+versions="1.6 1.7 1.8 1.9 master"
 for v in $versions
 do
   time clj -A:test:runner:$v
