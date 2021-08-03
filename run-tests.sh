f2eaf7efa31718763008ff66bfaac55c2d62e90c
Sean Corfield <sean@corfield.org>
2021-08-02 19:53:21 -0700

    clean up deps test setup

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-versions="1.6 1.7 1.8 1.9 1.10 master"
+versions="1.8 1.9 1.10 master"
 for v in $versions
 do
-  time clj -A:test:runner:$v
+  time clojure -M:test:$v
+  if test $? != 0; then exit 1; fi
 done
